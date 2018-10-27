799862c3f840ab7c45b268b2f89318751c220e0f
Joseph Cobhams <joseph.cobhams@andela.com>
2018-10-27 03:53:36 +0100

    - add a build create method - add unique key constraint - add required field condition - refaction findById and update methods

--- main/test/test.js
+++ main/test/test.js
@@ -3,7 +3,7 @@ import chai from 'chai';
 import DataModel from './../src/DummyDataModel';
 
 const { expect } = chai;
-const users = new DataModel('users');
+const users = new DataModel('users', ['name'], ['name', 'email']);
 const user1 = {
 	name: 'jane doe',
 	email: 'jane_doe@somebody.com',
@@ -14,19 +14,39 @@ const user2 = {
 	email: 'alice@somebody.com',
 	address: 'lives in another planet',
 }
-const createdUser2 = {
-	id: 2,
+
+const userWithoutEmail ={
+	name: 'alice',
+	noEmail: 'alice@somebody.com',
+	address: 'lives in another planet',
+}
+
+const bulkUsers = [
+	{
+		name: 'brain',
+		email: 'brain@email.com',
+		address: 'lives in pluto',
+	},
+	{
+		name: 'walter',
+		email: 'walter@email.com',
+		address: 'lives in mars',
+	},
+	{
+		name: 'ryan',
+		email: 'ryan@email.com',
+		address: 'lives in neptune',
+	},
+]
+const createdUser2 = {};
+const createdUser1 = {};
+const wrongdUser1 = {
+	id: 10,
 	name: 'alice',
 	email: 'alice@somebody.com',
 	address: 'lives in another planet',
 }
-const wrongdUser2 = {
-	id: 1,
-	name: 'alice',
-	email: 'alice@somebody.com',
-	address: 'lives in another planet',
-}
-const updateUser2 = {
+const userToUpdate1 = {
 	name: 'alice bob',
 	address: 'now living in planet earth',
 }
@@ -51,9 +71,10 @@ describe('Dummy Data Model', () => {
 		it('user model should be an array', () => {
 			expect(users.model).to.be.an('array');
 		})
-		it('it should a new user', () => {
+		it('should create a new user', () => {
 			users.create(user1)
 			.then((user) => {
+				Object.assign(createdUser1, user);
 				expect(user).to.eql({
 					id: 1,
 					name: 'jane doe',
@@ -63,9 +84,10 @@ describe('Dummy Data Model', () => {
 			});
 		});
 
-		it('it should a another user', () => {
+		it('should create a another user', () => {
 			users.create(user2)
 			.then((user) => {
+				Object.assign(createdUser2, user);
 				expect(user).to.eql({
 					id: 2,
 					name: 'alice',
@@ -74,15 +96,35 @@ describe('Dummy Data Model', () => {
 				});
 			});
 		});
+		it('should not create a model with unique key constriant', () => {
+			users.create(user1)
+			.catch((error) => {
+				expect(error).to.eql({ message: 'duplicate entry for unique key name' });
+			});
+		});
+
+		it('should not create a model if a required field is not present', () => {
+			users.create(userWithoutEmail)
+			.catch((error) => {
+				expect(error).to.eql({ message: `missing required field email` });
+			});
+		});
+
+		it('should create multiple users using bulk create', () => {
+			users.bulkCreate(bulkUsers)
+			.then(result => {
+				expect(result.length).to.equal(3)
+			})
+		})
 
 		it('lenght of model should increase', () => {
-			expect(users.model.length).to.equal(2);
+			expect(users.model.length).to.equal(5);
 		})
 	});
 
 	describe('update method', () => {
 		it('should update a model', () => {
-			users.update(createdUser2, updateUser2)
+			users.update(createdUser2, userToUpdate1)
 			.then((newUser2) => {
 				expect(newUser2).to.eql({
 					id: 2,
@@ -93,15 +135,7 @@ describe('Dummy Data Model', () => {
 			});
 		});
 		it('should not update a wrong model', () => {
-			users.update(wrongdUser2, updateUser2)
-			.then((newUser2) => {
-				expect(newUser2).to.eql({
-					id: 2,
-					email: 'alice@somebody.com',
-					name: 'alice bob',
-					address: 'now living in planet earth',
-				});
-			})
+			users.update(wrongdUser1, userToUpdate1)
 			.catch((error) => {
 				expect(error).to.eql({ message: `user not found` })
 			})
@@ -122,7 +156,7 @@ describe('Dummy Data Model', () => {
 		});
 
 		it('should return not found if model with given id is not not found', () => {
-			users.findById(3)
+			users.findById(10)
 			.then((user) => {
 				expect(user).to.eql({
 					id: 1,
@@ -138,6 +172,13 @@ describe('Dummy Data Model', () => {
 	});
 
 	describe('find', () => {
+		it('should return an error message if no where condition is specified', () => {
+			users.find()
+			.catch((error) => {
+				expect(error).to.eql({ message: `missing object propertiy 'where' to find model`})
+			})
+		});
+
 		it('should find a model that meet the given conditions', () => {
 			users.find({
 				where: {
@@ -180,7 +221,7 @@ describe('Dummy Data Model', () => {
 			users.findAll()
 			.then((allUsers) => {
 				expect(allUsers).to.be.an('array');
-				expect(allUsers.length).to.equal(2);
+				expect(allUsers.length).to.equal(5);
 			});
 		});
 
@@ -195,6 +236,18 @@ describe('Dummy Data Model', () => {
 				expect(allUsers).to.have.length.of.at.least(1);
 			});
 		});
+
+		it('should return an empty if no matching condition is found', () => {
+			users.findAll({
+				where: {
+					address: 'somewhere in the world a',
+				}
+			})
+			.then((allUsers) => {
+				expect(allUsers).to.be.an('array');
+				expect(allUsers.length).to.equal(0);
+			});
+		});
 	});
 
 	describe('destroy', () => {
@@ -206,7 +259,18 @@ describe('Dummy Data Model', () => {
 			})
 			.then((message) => {
 				expect(message).to.eql({ message: 'user has been deleted' });
+			});
+		});
+
+		it('should  not delete a model that does not meets the specified condition', () => {
+			users.destroy({
+				where: {
+					id: 5,
+				}
 			})
-		})
-	})
+			.catch((message) => {
+				expect(message).to.eql({ message: 'user not found, not action taken' });
+			});
+		});
+	});
 });
