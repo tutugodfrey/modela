94e878acbf84e3376dad00841598aba152b0c333
Godfrey Tutu <godfreytutu@Godfreys-MacBook-Pro.local>
2021-01-19 04:16:06 +0100

    Update findAll method
    - Add tests to ensure returnFields work as expected
    - Add tests to enforce are search condition is pass to the method

--- src/main/methods/findAll.js
+++ src/main/methods/findAll.js
@@ -6,11 +6,28 @@ function findAll(condition = 'all', returnFields=[]) {
     condition is single object with property where whose value is further
     an object with key => value pair of the properties of the object to find
   */
-  if (!Array.isArray(returnFields)) {
-    throw new TypeError('Expected an array of fields to return');
-  }
 
   const result = new Promise((resolve, reject)  => {
+    if (!Array.isArray(returnFields)) {
+      reject({ message: 'Expected an array of fields to return'});
+    }
+
+    let checkWhereKeys;
+    const checkAll = condition === 'all';
+    const checkWhere = condition.where;
+    const checkObjType = Object.prototype.toString.call(condition) === '[object Object]';
+    if (checkObjType && checkWhere) {
+      checkWhereKeys = Object.keys(condition.where) ? Object.keys(condition.where).length : false;
+    }
+
+    if ((!checkAll && !checkObjType) ||
+       (!checkAll && !checkWhere) ||
+       (checkWhere && !checkWhereKeys)) {
+        reject({ message:
+          'Expected an object with key \'where\' or the string \'all\' at position 1', 
+        });
+    }
+
     if (this.using_db) {
       const queryString = this.getQuery(this.modelName, condition, returnFields);
       return this.db_connection.query(queryString)
@@ -24,7 +41,15 @@ function findAll(condition = 'all', returnFields=[]) {
           return reject(err)
         });
     } else {
-    if (condition === 'all') resolve(this.model);
+    if (condition === 'all') {
+      if (!returnFields.length) resolve(this.model);
+      const model = this.model.map(model => {
+        const model_ = {};
+        returnFields.forEach(field => model[field] ? model_[field] = model[field] : null);
+        return model_;
+      });
+      return resolve(model);
+    }
     // array of objects that meet the condition
     const models = this.model.filter((model) => {
       const findMatchProps = confirmPropMatch(
